4a8e1183788cef1e2d99c11049305a0c724d859d
LeyHope <wesleymaiabls@gmail.com>
2022-08-11 15:11:41 -0300

    implementação do ResumosController

--- api/controllers/ResumosController.js
+++ api/controllers/ResumosController.js
@@ -1,6 +1,16 @@
 const database = require('../models')
-const { Op } = require('sequelize')
+const { Op, DatabaseError } = require('sequelize')
 const e = require('express')
+
+
+
+function analise(categoria, categoriaCadastrada, nomeCategoria, e) {
+    if(categoria === categoriaCadastrada){
+        nomeCategoria += parseInt(e.valor)
+        console.log(`${categoriaCadastrada}: ${nomeCategoria}`)
+    }
+    return nomeCategoria
+}
 
 class ResumosController {
 
@@ -9,7 +19,7 @@ class ResumosController {
 
         try {
             const consultaReceita = await database.Receitas.findAll({
-                attributes: ['valor'],
+                // attributes: ['valor'],
                 where: {
                     data: {
                         [Op.like]: `${ano}-${mes}-%`
@@ -25,22 +35,95 @@ class ResumosController {
                 }
             })
 
-
-
             let valorTotalReceita = 0
-
-
             consultaReceita.forEach(element => {
-                const valorDoElemento = element.valor
-                const converte = parseInt(valorDoElemento)
-                valorTotalReceita = valorTotalReceita + converte
+                let valorDoElemento = parseInt(element.valor)
+                valorTotalReceita = valorTotalReceita + valorDoElemento
             });
-            console.log(valorTotalReceita)
+
+            let valorTotalDespesa = 0
+            consultaDespesas.forEach(element => {
+                let valorDoElementoDespesa = parseInt(element.valor)
+                valorTotalDespesa = valorTotalDespesa + valorDoElementoDespesa
+            });
+
+            const balanco = valorTotalReceita - valorTotalDespesa
 
 
+            let alimentacao = 0
+            let saude = 0
+            let moradia = 0
+            let transporte = 0
+            let educacao = 0
+            let lazer = 0
+            let imprevistos = 0
+            let outros = 0
 
 
-            return res.status(200).json({Receitas: consultaReceita, Despesas: consultaDespesas})
+            consultaDespesas.forEach(e => {              
+                const categoria = e.categoria
+                if(categoria === 1) {
+                    alimentacao = alimentacao + parseInt(e.valor)
+                }
+                if(categoria === 2) {
+                    saude = saude + parseInt(e.valor)
+                }
+                if(categoria === 3) {
+                    moradia += parseInt(e.valor)
+                }
+                if(categoria === 4) {
+                    transporte += parseInt(e.valor)  
+                }
+                if(categoria === 5) {
+                    educacao += parseInt(e.valor)
+                }
+                if(categoria === 6) {
+                    lazer += parseInt(e.valor)
+                }
+                if(categoria === 7) {
+                    imprevistos += parseInt(e.valor)
+                }
+                if(categoria === 8) {
+                    outros += parseInt(e.valor)
+                }
+                
+            })
+
+            // console.log(`Alimentação: ${alimentacao}`)
+            // console.log(`Saúde: ${saude}`)
+            // console.log(`Moradia: ${moradia}`)
+            // console.log(`Transporte: ${transporte}`)
+            // console.log(`Educação: ${educacao}`)
+            // console.log(`Lazer: ${lazer}`)
+            // console.log(`Imprevistos: ${imprevistos}`)
+            // console.log(`Outros: ${outros}`)
+
+            const lista = [
+                {alimentacao},
+                {saude},
+                {moradia},
+                {transporte},
+                {educacao},
+                {lazer},
+                {imprevistos},
+                {outros}
+            ]
+
+            const novaLista = []
+
+            for(let i = 0; i<lista.length; i++){
+                if(Object.values(lista[i])>0){
+                  //console.log(Object.values(lista2[i]))
+                  let key = Object.keys(lista[i])
+                  let value = Object.values(lista[i])
+                  let adicionar = `${key}: ${value}`
+                  novaLista.push(adicionar)
+                }
+              }
+              console.log(novaLista)
+
+
+            return res.status(200).json({"Total de Receitas": valorTotalReceita, "Total de Despesas": valorTotalDespesa, "Saldo": balanco, "Despesas por categoria": novaLista})
 
         } catch (erro) {
             return res.status(400).json(erro.message)
@@ -48,6 +131,9 @@ class ResumosController {
 
 
     }
+
+
+
 
 }
 
